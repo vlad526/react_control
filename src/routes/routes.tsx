56623453fc07a2972e251c933bdd32dcac411d9c
vlad526 <vlad.kar765@gmail.com>
2026-03-20 19:26:57 +0200

    fix

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -21,8 +21,8 @@ export const routes = createBrowserRouter([
                     { path: "movie", element: <MoviesPage />},  ]
             },
             { path: "movie/:id", element: <MovieInfoPage /> },
-
-
         ],
     },
-]);
+], {
+    basename: "/react-control"
+});
